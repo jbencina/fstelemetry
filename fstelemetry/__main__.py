f428e22d0150f4adfc2b0303f45c8e18f45a7768
John <jbencina@users.noreply.github.com>
2021-01-08 15:51:57 -0800

    Changed default log file name to start datetime

--- fstelemetry/__main__.py
+++ fstelemetry/__main__.py
@@ -1,4 +1,5 @@
 from .telemetry import Telemetry
+from datetime import datetime
 import argparse
 import yaml
 
@@ -12,11 +13,12 @@ def get_keys(path):
     return keys
 
 def main():
+    logname = datetime.now().strftime('%Y%m%d%H%M%S') + '.csv'
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--config', default='config.yaml',
                         help='Relative path to the config file')
-    parser.add_argument('--log', default='log.csv',
+    parser.add_argument('--log', default=logname,
                         help='Relative path for the output log file')
     parser.add_argument('--interval', default=1.0,
                         help='Polling interval in seconds')
